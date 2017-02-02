31dda97d9d2a80402237393cae30a57c5a9ddf8a
Tamara Cook <tamara.cook@icloud.com>
2017-02-02 05:57:03 +0100

    Add providers to package manifest

--- Package.swift
+++ Package.swift
@@ -2,5 +2,9 @@ import PackageDescription
 
 let package = Package(
     name: "Clibsane",
-    pkgConfig: "sane-backends"
+    pkgConfig: "sane-backends",
+    providers: [
+        .Brew("sane-backends"),
+        .Apt("libsane")
+    ]
 )
